e43489c5f9b74c83f3e7b6c002a84350ab09a9e4
nonoLin <denkanono@gmail.com>
2025-01-31 16:54:22 +0800

    feat: add product template and basic select option

---
+++ container/Product/components/Detail/index.tsx
@@ -0,0 +1,112 @@
+import React, { useMemo, useState } from 'react'
+
+import Select from '@/components/Select'
+import type { Product, ProductItem, ProductOption } from '@/constants/types'
+
+interface DetailProps {
+  currentProduct: Product
+}
+
+const AMOUNT_BUTTON_CLASS =
+  'h-10 w-10 border-2 border-primary rounded-full bg-white font-bold lg:hover:bg-secondary lg:hover:text-white'
+
+const Detail = ({ currentProduct }: DetailProps) => {
+  const CURRENT_PRODUCT_DEFAULT = currentProduct?.items[0] || { options: [] }
+  const [selectedProduct, setSelectedProduct] = useState<ProductItem | undefined>(CURRENT_PRODUCT_DEFAULT)
+  const [selectedOption, setSelectedOption] = useState<ProductOption | undefined>(CURRENT_PRODUCT_DEFAULT.options[0])
+  const [amount, setAmount] = useState(1)
+  const discountPrice = selectedOption ? selectedOption.price * 0.9 : 0
+
+  const onProductChange = (productId: string) => {
+    const product = currentProduct.items.find((item) => item.id === productId)
+    if (!product) {
+      console.error(`Product ${productId} not found`)
+      return
+    }
+
+    setSelectedProduct(product)
+    setSelectedOption(product.options[0])
+  }
+
+  const onSizeChange = (size: number) => {
+    if (!selectedProduct) return
+    const option = selectedProduct.options.find((option) => option.size === size)
+
+    if (!option) {
+      console.error(`Size ${size} not found for product ${selectedProduct.name}`)
+      return
+    }
+    setSelectedOption(option)
+  }
+
+  const onAmountChange = (count: number) => {
+    if (amount === 1 && count === -1) return
+    setAmount((preAmount) => preAmount + count)
+  }
+
+  const details = useMemo(() => currentProduct.details, [currentProduct.details])
+
+  const productOptions = useMemo(
+    () =>
+      currentProduct.items.map((item) => ({
+        label: item.name,
+        value: item.id,
+      })),
+    [currentProduct.items]
+  )
+
+  const sizeOptions = useMemo(
+    () =>
+      selectedProduct?.options.map((option) => ({
+        label: `${option.size} (ml/g)`,
+        value: option.size,
+      })) || [],
+    [selectedProduct]
+  )
+
+  return (
+    <div className="w-full flex flex-col gap-5 px-4 text-fontColor lg:w-lg">
+      <div className="text-2xl font-bold lg:text-3xl">{currentProduct.name}</div>
+
+      <div className="flex items-baseline text-3xl lg:text-4xl">
+        <span className="text-2xl font-bold">NT$</span>
+        <span className="mx-1 text-4xl text-warningColor font-bold">{discountPrice}</span>
+        <span className="text-2xl text-zinc-500 line-through">{selectedOption?.price}</span>
+      </div>
+
+      <Select options={productOptions} onChange={onProductChange} />
+
+      <Select options={sizeOptions} onChange={onSizeChange} value={selectedOption?.size} />
+
+      <div className="mb-4 flex flex-col gap-4">
+        <div className="flex items-center justify-evenly">
+          <button className={`${AMOUNT_BUTTON_CLASS}`} onClick={() => onAmountChange(-1)}>
+            -
+          </button>
+          <span className="text-lg">{amount}</span>
+          <button className={`${AMOUNT_BUTTON_CLASS}`} onClick={() => onAmountChange(1)}>
+            +
+          </button>
+        </div>
+        <button className="rounded bg-primary px-3 py-2 text-white tracking-widest hover:bg-secondary">
+          加入購物車
+        </button>
+      </div>
+
+      <ul className="flex flex-col gap-2 border-t-1 border-gray-300">
+        <div className="my-4 text-lg font-bold lg:text-2xl">Description</div>
+        {details.map((detail, index) => (
+          <li key={index}>
+            {detail.map((content, contentIdx) => (
+              <p className={contentIdx === 0 ? 'text-primary font-bold' : 'ml-3'} key={content + contentIdx}>
+                {content}
+              </p>
+            ))}
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default Detail
--- container/Product/index.tsx
+++ container/Product/index.tsx
@@ -1,9 +1,60 @@
+import Image from 'next/image'
 import { useRouter } from 'next/router'
+import React, { useMemo } from 'react'
+import { MdArrowBackIos, MdAutorenew } from 'react-icons/md'
+
+import { PRODUCTS } from '@/constants/products'
+import type { Product } from '@/constants/types'
+import Detail from '@/container/Product/components/Detail'
+
 const Product = () => {
   const router = useRouter()
-  console.log(router)
+  const id = router.query.slug
+  const currentProduct = PRODUCTS.find((product) => product.id === id)
+  const imageUrl = useMemo(() => `/assets/images/products/${currentProduct?.id}.webp`, [currentProduct?.id])
 
-  return <div>Product</div>
+  const backToPreviousPage = () => {
+    router.back()
+  }
+  if (!currentProduct) {
+    return (
+      <div className="h-screen flex items-center justify-center">
+        <MdAutorenew size={30} className="animate-spin animate-duration-1s animate-count-infinite" />
+      </div>
+    )
+  }
+
+  return (
+    <>
+      <div
+        className="text-fontColorLight mb-2 flex cursor-pointer items-center text-3xl font-bold hover:text-secondary"
+        onClick={backToPreviousPage}
+      >
+        <MdArrowBackIos size={30} />
+        <span>Back</span>
+      </div>
+      <div className="flex flex-col gap-4 lg:flex-row lg:px-6xl">
+        <div className="flex flex-1 justify-center lg:justify-end">
+          <div className="bg-white lg:h-lg lg:w-lg">
+            <Image
+              src={imageUrl}
+              alt={currentProduct.name}
+              width={400}
+              height={400}
+              style={{
+                width: '100%',
+                height: '100%',
+              }}
+              className=""
+            />
+          </div>
+        </div>
+        <div className="flex-1">
+          <Detail currentProduct={currentProduct} />
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Product
