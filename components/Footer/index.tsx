39d5a1b680d5f4b2a82eef55941c91446fdd3e35
nonoLin <denkanono@gmail.com>
2025-02-12 19:00:57 +0800

    refactor: modify text sm

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="h-16 w-full flex items-center justify-center bg-secondary text-default">
+    <footer className="h-16 w-full flex items-center justify-center bg-secondary text-xs text-default lg:text-base">
       &#169; 2025 Ona & Alina All Rights Reserved &#10072; Developed by Nono.L &#9786;
     </footer>
   )
